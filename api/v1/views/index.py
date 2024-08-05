904631aa61b050307d9afc07144904e46272b26a
Katalyst99 <katleho333@gmail.com>
2024-08-05 02:13:05 -0700

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """A file index.py"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -9,3 +10,18 @@ def status():
     """Returns json status"""
     json_resp = jsonify(status='OK')
     return json_resp
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type"""
+    type_cls = {
+           "amenities": storage.count('Amenity'),
+           "cities": storage.count('City'),
+           "places": storage.count('Place'),
+           "reviews": storage.count('Review'),
+           "states": storage.count('State'),
+           "users": storage.count('User'),
+    }
+
+    return jsonify(type_cls)
